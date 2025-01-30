91ed1bd904cc3e0ef8713fd61a560ef1e54bca09
Patrick Kačič <patrick.kacic@teleport-studio.com>
2025-01-30 12:02:38 +0100

    add hero

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+const header = ref()
+
+onMounted(() => {
+  setTimeout(() => {
+    header.value.style.transform = 'translateY(0)'
+  }, 1000)
+})
+</script>
+
+<template>
+  <header
+    ref="header"
+    class="fixed z-50 top-4 left-4 right-4 flex items-center justify-between px-6 h-16 bg-white/30 backdrop-blur-[10px] rounded-[10px] shadow-[0_18px_40px_rgba(0,0,0,0.03),0_0_2px_rgba(0,0,0,0.14)] bg-blur max-w-[2000px] mx-auto"
+  >
+    <img src="../assets/logo.png" alt="logo" class="h-8 w-8" />
+    <div class="hidden items-center gap-4 lg:flex">
+      <router-link
+        to="/"
+        class="px-4 py-2 hover:bg-[#ededed] rounded-[10px]"
+        activeClass="bg-[#ededed] rounded-[10px]"
+      >
+        <span class="leading-[18px]">Home</span>
+      </router-link>
+      <router-link
+        to="/services"
+        class="px-4 py-2 hover:bg-[#ededed] rounded-[10px]"
+        activeClass="bg-[#ededed] rounded-[10px]"
+      >
+        <span class="leading-[18px]">What we do</span>
+      </router-link>
+      <router-link
+        to="/about"
+        class="px-4 py-2 hover:bg-[#ededed] rounded-[10px]"
+        activeClass="bg-[#ededed] rounded-[10px]"
+      >
+        <span class="leading-[18px]">About</span>
+      </router-link>
+    </div>
+    <div class="relative inline-flex group">
+      <div
+        class="absolute transitiona-all duration-1000 opacity-60 -inset-px bg-gradient-to-r from-[#8A2BE2] via-[#8A2BE2] to-[#8A2BE2] rounded-[10px] blur-md group-hover:opacity-80 group-hover:-inset-1 group-hover:duration-200 animate-tilt"
+      ></div>
+      <button
+        type="button"
+        class="relative inline-flex items-center justify-center px-4 py-2 text-white transition-all duration-200 bg-[#8A2BE2] rounded-[10px] focus:outline-none"
+      >
+        <span class="leading-[18px]">Let's work</span>
+      </button>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  transform: translateY(-150px);
+  transition: 0.5s;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,91 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { computed, onMounted, ref } from 'vue'
+import { gsap } from 'gsap'
+import { usePointer } from '@vueuse/core'
+
+const { x, y } = usePointer()
+
+const heroBg = ref()
+
+const transformBg = computed(() => {
+  const offsetX = (x.value - window.innerWidth / 2) * 0.04
+  const offsetY = (y.value - window.innerHeight / 2) * 0.04
+
+  return `translate3d(${offsetX}px, ${offsetY}px, 0)`
+})
+
+onMounted(() => {
+  gsap.to('.char', {
+    duration: 0.1,
+    y: 0,
+    delay: 0.1,
+    stagger: 0.015,
+  })
+  gsap.to('.hero-text', {
+    duration: 0.1,
+    y: 0,
+    delay: 1,
+  })
+  gsap.fromTo(
+    heroBg.value,
+    { opacity: 0 },
+    {
+      opacity: 1,
+      duration: 1,
+      delay: 1,
+      ease: 'power2.out',
+    },
+  )
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section class="h-svh">
+    <div
+      ref="heroBg"
+      class="absolute z-[-1] h-[150vh] w-full max-w-[1000px] min-w-[500px] top-[40vh] left-[50%] translate-x-[-50%] border-[6rem] border-b-0 md:border-[9rem] md:border-b-0 rounded-tl-[100%] rounded-tr-[100%] rounded-bl-[100%] rounded-br-[100%] border-[#000] blur-xl md:blur-2xl"
+      :style="{ transform: transformBg }"
+    ></div>
+    <div class="flex flex-col h-full items-center justify-start text-black w-full hero-text">
+      <h1
+        class="w-full text-center max-w-[2000px] whitespace-nowrap font-bold mt-[120px] mb-auto leading-tight"
+        style="font-size: clamp(2rem, 12vw, 16rem)"
+        :style="{
+          clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+        }"
+      >
+        <div class="inline-block char">T</div>
+        <div class="inline-block char">e</div>
+        <div class="inline-block char">l</div>
+        <div class="inline-block char">e</div>
+        <div class="inline-block char">p</div>
+        <div class="inline-block char">o</div>
+        <div class="inline-block char">r</div>
+        <div class="inline-block char">t</div>
+        <div class="inline-block char">&nbsp;</div>
+        <div class="inline-block char">S</div>
+        <div class="inline-block char">t</div>
+        <div class="inline-block char">u</div>
+        <div class="inline-block char">d</div>
+        <div class="inline-block char">i</div>
+        <div class="inline-block char">o</div>
+      </h1>
+      <p class="text-center font-extralight lg:text-[20px] mb-[10%]">
+        a creative digital studio<br />
+        based in Maribor, Slovenia
+      </p>
+    </div>
+  </section>
+  <section class="h-[2000px]"></section>
 </template>
+
+<style scoped>
+.char {
+  transform: translateY(13vw);
+  transition: transform 0.3s;
+}
+.hero-text {
+  transform: translateY(30%);
+  transition: transform 0.5s;
+}
+</style>
