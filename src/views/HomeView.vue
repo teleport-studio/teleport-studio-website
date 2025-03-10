64483b033f7e74129df1c931bec601379f952a5e
Patrick Kačič <patrick.kacic@teleport-studio.com>
2025-03-10 22:42:06 +0100

    fix type error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,12 +29,14 @@ const handleMouseMoveSection5 = (e: MouseEvent) => {
   const cards = cardsRef.value.querySelectorAll('.card')
 
   cards.forEach((card) => {
-    const rect = card.getBoundingClientRect()
-    const x = e.clientX - rect.left
-    const y = e.clientY - rect.top
+    if (card instanceof HTMLElement) {
+      const rect = card.getBoundingClientRect()
+      const x = e.clientX - rect.left
+      const y = e.clientY - rect.top
 
-    card.style.setProperty('--mouse-x', `${x}px`)
-    card.style.setProperty('--mouse-y', `${y}px`)
+      card.style.setProperty('--mouse-x', `${x}px`)
+      card.style.setProperty('--mouse-y', `${y}px`)
+    }
   })
 }
 
